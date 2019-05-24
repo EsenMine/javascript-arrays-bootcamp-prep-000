b7f1b7a68e236a00319d67819cf642855f7e6d04
Miné E <mine_esen@yahoo.ca>
2019-05-24 14:40:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ['snickers','hundred grand', 'kitkat','skittles'];
 
 function addElementToBeginningOfArray(array,element){
+  newArray = [element
   array.unshift(element);
   return array;
 }
